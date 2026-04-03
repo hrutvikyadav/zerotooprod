dd8f2a3d52e93e36d74c331c4725a2c4abf885a9
hrutvikyadav <hrutvikyadav@gmail.com>
2026-04-03 14:45:59 +0530

    feat(telemetry): instrument subscribe
    - simplest log macros

--- src/routes/subscribe.rs
+++ src/routes/subscribe.rs
@@ -9,6 +9,12 @@ pub struct FormData {
     name: String,
 }
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    // GDPR!!!
+    log::info!(
+        "Saving new subscriber details ({}, {}) in the database",
+        form.email,
+        form.name
+    );
     match sqlx::query!(
         r#" INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -23,9 +29,14 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("Subscriber details saved to database");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            // note the Debug fmt specifier to capture rich information
+            // which is stripped off by Display
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
