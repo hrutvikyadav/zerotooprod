4c5e905bf5b598c8206b242b300eeb5d98ef0767
hrutvikyadav <hrutvikyadav@gmail.com>
2026-04-07 16:01:44 +0530

    fix: tracing log
    - redir 3rd party lib log macros to tracing subscriber for processing

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use env_logger::Env;
 use sqlx::PgPool;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zerotooprod::configuration::get_configuration;
 use zerotooprod::startup::run;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
+    LogTracer::init().expect("Failed to set logger");
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new("zerotooprod".into(), std::io::stdout);
     let subscriber = Registry::default()
